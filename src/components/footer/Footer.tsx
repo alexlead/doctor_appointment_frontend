841789c87da889dd28f0ca9df39096e801dff605
Kira <zaplatynskakira@gmail.com>
2024-03-29 15:33:36 +0100

    feature(Home): add Footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="text-end p-3">
+      Copyright © 2024. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,13 @@ import HomeHeader from '../components/HomeHeader';
 import RegistrationWindow from '../components/RegistrationWindow';
 import LoginFormLaunch from '../components/LoginFormLaunch';
 import Banner from '../components/banner/Banner'; 
-
+import Footer from '../components/footer/Footer';
 const Home = () => {
   return (
     <div>
       <HomeHeader />
       <Banner />
+      <Footer />
     </div>
   );
 };
